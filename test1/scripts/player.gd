d442e7e32a8dd2a4787635cabf05bfcbdfe0f1dd
radityadias <radityadias24@gmail.com>
2025-06-06 13:00:33 +0700

    add player state logic

--- test1/scripts/player.gd
+++ test1/scripts/player.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
 
+enum {
+	IDLE, 
+	RUN,
+	JUMP,
+	DOUBLE_JUMP,
+	PUSH,
+	PULL
+}
+
 # PLAYER MECHANIC
 @export var SPEED : float = 250
 @export var ACCELERATION : float = 600
@@ -7,6 +16,7 @@ extends CharacterBody2D
 @export var JUMP_FORCE : float = -350
 @export var PUSH_FORCE : float = 200
 var JUMP_COUNT : int = 0
+var state : int = IDLE
 
 # NODES
 @onready var character: AnimatedSprite2D = $Character
@@ -14,7 +24,7 @@ var JUMP_COUNT : int = 0
 @onready var run_particle: AnimatedSprite2D = $"Run Particle"
 @onready var dust = preload("res://scenes/players/jump_particle.tscn")
 
-# CUSTOM VARIABLES
+# Physics
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_grounded: bool = true
 
@@ -31,13 +41,34 @@ func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("move_left", "move_right") # Get input direction (-1, 0, or 1)
 	
 	apply_gravity(delta)
-	apply_movement(direction, delta)
-	apply_jump_logic()
-	push_pull_logic()
+	handle_state(direction, delta)
 	apply_dust()
 	apply_animation(direction)
 
 	move_and_slide()
+	update_state(direction)
+
+func handle_state(direction: float, delta: float) -> void:
+	match state:
+		IDLE, RUN:
+			apply_movement(direction, delta)
+			try_jump()
+			try_push_pull()
+		JUMP, DOUBLE_JUMP:
+			apply_movement(direction, delta)
+			try_double_jump()
+		PUSH, PULL:
+			handle_push_pull(direction)
+
+func update_state(direction: float) -> void:
+	if is_on_floor() and state in [JUMP, DOUBLE_JUMP]:
+		state = IDLE if abs(velocity.x) < 1.0 else RUN
+		JUMP_COUNT = 0
+	elif is_on_floor():
+		if direction != 0 and state not in [PUSH, PULL]:
+			state = RUN
+		elif direction == 0 and state not in [PUSH, PULL]:
+			state = IDLE
 
 func apply_gravity(delta: float):
 	# Apply gravity and handle coyote time
@@ -45,56 +76,68 @@ func apply_gravity(delta: float):
 		velocity.y += GRAVITY * delta
 	else:
 		coyote_timer = COYOTE_TIME  # Reset coyote timer when on ground
-		JUMP_COUNT = 0
 
 func apply_movement(direction: float, delta: float):
 	# Horizontal movement
 	if direction != 0: 
 		velocity.x = move_toward(velocity.x, direction * SPEED, ACCELERATION * delta * 2)
-	else: 
-		velocity.x = move_toward(velocity.x, 0, FRICTION * delta * 2)
-
-func apply_jump_logic():
-	# Check jump conditions
-	
-	# Single Jump
-	if (jump_buffer_timer > 0 or coyote_timer > 0) and JUMP_COUNT == 0 and Input.is_action_just_pressed("jump") :
-		apply_jump()
-		JUMP_COUNT += 1
-		print("JUMP COUNT : ", JUMP_COUNT)
-	
-	# Double Jump
-	elif Input.is_action_just_pressed("jump") and not is_on_floor() and JUMP_COUNT < 2:
-		apply_jump()
-		JUMP_COUNT += 1
-		print("JUMP COUNT : ", JUMP_COUNT)
-
-func apply_jump() -> void:
-	velocity.y = JUMP_FORCE
-	jump_buffer_timer = 0  # Clear jump buffer
-	coyote_timer = 0  # Disable coyote time after jumping
-
-func apply_animation(direction: float):
-	# Flip sprites based on movement direction
-	if direction != 0:
 		character.flip_h = direction < 0
 		run_particle.flip_h = direction < 0
 		run_particle.position.x = abs(run_particle.position.x) * (-1 if direction > 0 else 1)
+	else: 
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta * 2)
 
-	if is_on_floor():
-		if direction != 0: # RUN
-			character.play("Run")
-			run_particle.play("running_dust")
-			animation_player.play("run")
-		else: # IDLE
+func try_jump():
+	if (jump_buffer_timer > 0 or coyote_timer > 0) and Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_FORCE
+		state = JUMP
+		JUMP_COUNT += 1
+		jump_buffer_timer = 0
+		coyote_timer = 0
+
+func try_double_jump() -> void:
+	if (Input.is_action_just_pressed("jump") and JUMP_COUNT < 2):
+		velocity.y =JUMP_FORCE
+		state = DOUBLE_JUMP
+		JUMP_COUNT += 1
+
+func try_push_pull() -> void:
+	if Input.is_action_just_pressed("interact"):
+		for i in get_slide_collision_count():
+			var c = get_slide_collision(i)
+			
+			if c.get_collider() is RigidBody2D:
+				state = PUSH if velocity.x != 0 else PULL
+				return
+
+func handle_push_pull(direction: float) -> void:
+	if Input.is_action_pressed("interact"):
+		for i in get_slide_collision_count():
+			var c = get_slide_collision(i)
+			
+			if c.get_collider() is RigidBody2D:
+				c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE)
+	else:
+		state = IDLE if abs(velocity.x) < 1.0 else RUN
+
+func apply_animation(direction: float):
+	match state:
+		IDLE: 
 			character.play("Idle")
 			animation_player.play("idle")
 			run_particle.stop()
-	else: # JUMP
-		if character.animation != "Jump":
-			character.play("Jump")
-		animation_player.play("jump")
-		run_particle.stop()
+		RUN: 
+			character.play("Run")
+			run_particle.play("running_dust")
+			animation_player.play("run")
+		JUMP, DOUBLE_JUMP:
+			if character.animation != "Jump":
+				character.play("Jump")
+			animation_player.play("jump")
+			run_particle.stop()
+		PUSH, PULL:
+			character.play("Push")  # You'll need to create this animation
+			run_particle.stop()
 
 func apply_dust():
 	var instance = dust.instantiate()
@@ -113,17 +156,8 @@ func apply_dust():
 
 func update_timers(delta: float):
 	# Reduce timers
-	if coyote_timer > 0:
-		coyote_timer -= delta
-	if jump_buffer_timer > 0:
-		jump_buffer_timer -= delta
+	if coyote_timer > 0: coyote_timer -= delta
+	if jump_buffer_timer > 0: jump_buffer_timer -= delta
 
 	# Store jump input in buffer
-	if Input.is_action_just_pressed("jump"):
-		jump_buffer_timer = JUMP_BUFFER_TIME
-
-func push_pull_logic() -> void:
-	for i in get_slide_collision_count():
-		var c = get_slide_collision(i)
-		if c.get_collider() is RigidBody2D and Input.is_action_pressed("interact"):
-			c.get_collider().apply_central_impulse(-c.get_normal() * PUSH_FORCE)
+	if Input.is_action_just_pressed("jump"):jump_buffer_timer = JUMP_BUFFER_TIME
