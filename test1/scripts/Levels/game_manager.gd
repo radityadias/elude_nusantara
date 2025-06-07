0d77169c6856811afc8828ac899014b3eb712bc7
radityadias <radityadias24@gmail.com>
2025-06-07 13:35:36 +0700

    add simple traps damage

--- test1/scripts/Levels/game_manager.gd
+++ test1/scripts/Levels/game_manager.gd
@@ -2,9 +2,17 @@ extends Node
 
 var cards_collected : int = 0
 var is_having_card: bool = false
+var health = 3
 
 signal cards_changed(value: int)
 signal scanner_validated(value: bool)
+
+func decrease_health() -> void:
+	health -= 1
+	print(health)
+	
+	if health == 0 :
+		get_tree().reload_current_scene()
 
 func add_card():
 	cards_collected += 1
