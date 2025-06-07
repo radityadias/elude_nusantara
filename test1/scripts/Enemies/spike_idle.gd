490f12645cf284022ddc4ad20b51dadecd576482
radityadias <radityadias24@gmail.com>
2025-06-07 14:09:28 +0700

    little rearrangement

--- test1/scripts/Enemies/spike_idle.gd
+++ test1/scripts/Enemies/spike_idle.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@onready var game_manager: Node = %GameManager
+@onready var game_manager: Node = $"../../GameManager"
 
 func _on_body_entered(body: Node2D) -> void:
 	if(body.is_in_group("player")):
