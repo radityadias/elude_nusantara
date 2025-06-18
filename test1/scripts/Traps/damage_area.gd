0e22ca0fa5ea88573e7ddc8407a6322548cb3923
radityadias <radityadias24@gmail.com>
2025-06-19 05:56:58 +0700

    fix player invincible bug

--- test1/scripts/Traps/damage_area.gd
+++ test1/scripts/Traps/damage_area.gd
@@ -18,7 +18,8 @@ func _physics_process(delta: float) -> void:
 				game_manager.player_damaged()
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player") and not game_manager.player_is_dead:
+	var player = get_tree().get_first_node_in_group("player")
+	if body.is_in_group("player") and not game_manager.player_is_dead and not player.is_invincible:
 		is_inside = true
 		damage_timer = 0.0
 		game_manager.player_damaged()
